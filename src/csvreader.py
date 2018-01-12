29ede68e75580df56b9d54951715882d61e2bf2e
a259l <a259li@uwaterloo.ca>
2018-01-12 03:24:35 -0500

    added csv URL image loader for gathering trainign sets

--- src/csvreader.py
+++ src/csvreader.py
@@ -4,7 +4,8 @@ import urllib
 with open("../data/painting_dataset.csv") as f:
     reader = csv.reader(f)
     i=1
+    reader.next()
     for row in reader:
         i+=1
-        string = row[2]
-        f = open("../data/images/"+str(i).zfill(5))
+        string = row[2][1:-1]
+        urllib.urlretrieve(string, "../data/painting_images/" + str(i).zfill(5) + ".jpg")
